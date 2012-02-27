f1e1ea506614a65cf4714c0d909058bc6b5cab72
Kim Åström <kim.astrom@gmail.com>
2012-02-27 11:52:03 +0100

    clean up

--- js/collections/tasks.js
+++ js/collections/tasks.js
@@ -8,37 +8,36 @@ define([
 			url: '/',
 			model: Task,
 
-			/**
-			 * initializes the collections
-			 * @param List object
-			 */
-			initialize: function(model) {
-				this.listID = model.id;
-				//create storage with listID as key
-				this.localStorage = new Store(model.id);
-			},
+            /**
+             * initializes the collections
+             * @param List object
+             */
+            initialize: function(model) {
+                this.listID = model.id;
+                //create storage with listID as key
+                this.localStorage = new Store(model.id);
+            },
 
-			/**
-			 * filter the collection of done tasks
-			 */
-			done: function() {
-				return this.filter(function(task){ return task.get('done'); });
-			},
+            /**
+             * filter the collection of done tasks
+             */
+            done: function() {
+                return this.filter(function(task){ return task.get('done'); });
+            },
 
-			/**
-			 * sorts the collection by done status
-			 */
-			comparator: function(task) {
-			    return task.get('done');
-			},
+            /**
+             * sorts the collection by done status
+             */
+            comparator: function(task) {
+                return task.get('done');
+            },
 
-			/**
-			 * delete the localstorage
-			 */
-			destroy: function() {
-				localStorage.removeItem(this.listID);
-			}
-
-		});
-	return TaskCollection;
+            /**
+             * delete the localstorage
+             */
+            destroy: function() {
+            localStorage.removeItem(this.listID);
+            }
+        });
+    return TaskCollection;
 });
